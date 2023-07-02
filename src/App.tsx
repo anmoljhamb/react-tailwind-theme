1257a2cdd8c9fd3a4367c588746d10224385b6df
Anmol Jhamb <talktoanmol@outlook.com>
2023-07-02 21:11:22 +0530

    CSS Variables added. Added those variables in tailwind.config.js and modified the App.tsx layout to use those variables instead

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,14 @@ function App() {
     ) as ThemeContextInterface;
 
     return (
-        <div className="flex h-screen w-screen items-center justify-center bg-slate-950 text-white">
-            <div className="rounded-md bg-white p-8 text-black">
+        <div className="flex h-screen w-screen items-center justify-center bg-bgColor text-textColor">
+            <div className="rounded-md bg-textColor p-8 text-bgColor">
                 <h1 className="text-3xl">
                     Theme {darkTheme ? "dark" : "light"}
                 </h1>
                 <button
                     onClick={toggleTheme}
-                    className="mt-2 w-full rounded-none bg-slate-600 p-2 text-2xl text-white transition-all duration-300 ease-in-out hover:rounded-lg"
+                    className="mt-2 w-full rounded-none bg-bgColor p-2 text-center text-2xl uppercase tracking-[3px] text-textColor transition-all duration-300 ease-in-out hover:rounded-lg"
                 >
                     Toggle
                 </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                bgColor: `var(--bgColor)`,
+                textColor: `var(--textColor)`,
+            },
+        },
     },
     plugins: [],
 };
