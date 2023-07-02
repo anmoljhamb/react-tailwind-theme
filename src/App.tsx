46d947c5520ef0c636ae9b947a74b2e213ea071d
Anmol Jhamb <talktoanmol@outlook.com>
2023-07-02 20:54:13 +0530

    testing if the context works or not. And wrapping the entire App.tsx with ThemeProvider in main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { useContext } from "react";
+import { ThemeContext } from "./contexts";
+import { ThemeContextInterface } from "./types";
+
 function App() {
+    const { darkTheme, toggleTheme } = useContext(
+        ThemeContext
+    ) as ThemeContextInterface;
+
     return (
         <div className="flex h-screen w-screen items-center justify-center bg-slate-950 text-white">
-            <h1>Hello World</h1>
+            <div className="rounded-md bg-white p-8 text-black">
+                <h1 className="text-3xl">
+                    Theme {darkTheme ? "dark" : "light"}
+                </h1>
+                <button
+                    onClick={toggleTheme}
+                    className="mt-2 w-full rounded-none bg-slate-600 p-2 text-2xl text-white transition-all duration-300 ease-in-out hover:rounded-lg"
+                >
+                    Toggle
+                </button>
+            </div>
         </div>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "./providers";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <>
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
+    </>
+);
